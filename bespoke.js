269df652cae59832b6a6f4a37a287344ee30e07e
Stephanie <stephanie@curiousminds.com>
2013-09-06 16:12:50 -0700

    Added Google Places Autocomplete functionality to input boxes

--- bespoke.js
+++ bespoke.js
@@ -1,3 +1,5 @@
+var bounds;
+
 function createWrapper(element) {
 	var nextElement = element.nextSibling.nextSibling;
 
@@ -48,13 +50,23 @@ function addRestaurantField() {
 	addEvent(deleteButton, 'click', function() {
 		fieldset.removeChild(wrapper);
 	});
+
+	if (bounds) {
+		restaurantInputSearch(bounds);
+	}
 }
 
 function removeWrapper() {
 	var group = document.getElementsByTagName('span');
 	var fieldset = document.getElementById('custom-restaurants');
 
-	for (var tagCount = 0; tagCount < group.length; tagCount++) {
+	/*checkClass(group, 'delete',
+		addEvent(this, 'click', function(parentOfGroup) {
+			console.log(parentOfGroup)
+			parentOfGroup.removeChild(this);
+		}), fieldset);
+
+*/	for (var tagCount = 0; tagCount < group.length; tagCount++) {
 		if (group[tagCount].className.indexOf('delete') !== -1) {
 			addEvent(group[tagCount], 'click', function() {
 				fieldset.removeChild(this.parentNode);
@@ -63,13 +75,88 @@ function removeWrapper() {
 	}
 }
 
+function checkClass(elementList, classToCheck, callback, callbackParams) {
+	for (var count = 0; count < elementList.length; count++) {
+		if (elementList[count].className.indexOf(classToCheck) !== -1) {
+			console.log(callbackParams);
+			callback.call(elementList[count], callbackParams);
+		}
+	}
+}
+
 function createPage() {
 	var page = window.open('');
 	var location = document.getElementById('location-input').value;
-	page.document.write('<!DOCTYPE html><html><head>' +
+	var inputList = document.getElementsByTagName('input');
+	var restaurants = [];
+	var count = 0;
+
+	page.document.write(
+		'<!DOCTYPE html><html><head>' +
 		'<link rel="stylesheet" type="text/css" href="style.css" /></head><body><h1>' + location + 
-		'</h1></body><script type="text/javascript" src="wehoeatmo.js"></script></html>');
+		' Eat Mo</h1><h2>Help me pick a restaurant!</h2><div class="spinner"><ol id="restaurant-list">');
+
+	for (count = 0; count < inputList.length; count++) {
+		if (inputList[count].className.indexOf('restaurant-name') !== -1 && inputList[count].value.length > 0) {
+			restaurants.push(inputList[count].value);
+		}
+	}
+
+	for (count = 0; count < restaurants.length; count++) {
+		page.document.write('<li>' + restaurants[count] + '</li>');
+	}
+
+	page.document.write('</ol></div>' +
+		'<button id="pick-restaurant">Tell me where to go</button>' +
+		'<footer><p>Built with <a href="http://www.wehoeatmo.com">WeHo Eat Mo</p></footer>' +
+		'<script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?libraries=places&sensor=true"></script>' +
+		'</body><script type="text/javascript" src="wehoeatmo.js"></script></html>');
+
+
 }
+
+function getCity() {
+	var placeInput = document.getElementById('location-input');
+	var placeOptions = {
+		types: ['(cities)'],
+		componentRestrictions: {country: 'us'}
+	};
+
+	var autocompletePlace = new google.maps.places.Autocomplete(placeInput, placeOptions);
+	var city;
+
+	google.maps.event.addListener(autocompletePlace, 'place_changed', function() {
+		var place = autocompletePlace.getPlace();
+
+		console.log('bazooka');
+
+		if (place.geometry.location) {
+			console.log('boo');
+			bounds = new google.maps.LatLngBounds();
+			bounds.extend(place.geometry.location);
+
+			restaurantInputSearch(bounds);
+		}
+	})
+}
+
+function restaurantInputSearch(cityBounds) {
+	var restaurantNames = document.getElementsByTagName('input');
+
+	var restaurantOptions = {
+		types: ['establishment'],
+		bounds: cityBounds
+	}
+
+	for (var count = 0; count < restaurantNames.length; count++) {
+		if (restaurantNames[count].className.indexOf('restaurant-name') !== -1) {
+			var autocompleteRestaurant = new google.maps.places.Autocomplete(restaurantNames[count], restaurantOptions);
+			
+		}
+	}
+}
+
+
 
 var labels = document.getElementsByTagName('label');
 
@@ -80,5 +167,7 @@ for (var i = 0; i < labels.length; i++) {
 }
 
 removeWrapper();
+
+addEvent(document.getElementById('location-input'), 'click', getCity);
 
 addEvent(document.getElementById('preview'), 'click', createPage);
